fbecb269c00ca9194d946b111b3ff0a8c44a71c5
Daniel O'Connell <tojad99+github@gmail.com>
2016-03-06 23:09:50 +0100

    Get rid of database credentials in the code

--- leaflets/etc/heroku.py
+++ leaflets/etc/heroku.py
@@ -1,12 +1,21 @@
 import os
+import re
 
 
 PORT = int(os.environ.get('PORT'))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DB_NAME = 'd2de9evfrj5u2p'
-DB_USER = 'hodvexdtuufsju'
-DB_PASSWORD = 'kp6cjwLeBknrZo8Tk35h1nlcXW'
-DB_HOST = 'ec2-54-83-12-22.compute-1.amazonaws.com'
-DB_PORT = 5432
+database_url = os.environ.get('DATABASE_URL')
+
+database_parts = re.search(
+    r'postgres://(?P<user>.*?):(?P<password>.*?)@(?P<host>.*?):(?P<port>.*?)/(?P<database>.*?)$',
+    database_url
+)
+
+if database_parts:
+    DB_NAME = database_parts.group('database')
+    DB_USER = database_parts.group('user')
+    DB_PASSWORD = database_parts.group('password')
+    DB_HOST = database_parts.group('host')
+    DB_PORT = int(database_parts.group('port') or 5432)
 
